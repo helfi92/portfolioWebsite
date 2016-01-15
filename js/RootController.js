956cc75e9ad58ea384a9f100eb6639dc52d23f7c
Hassan Ali <helfi92@gmail.com>
2016-01-15 16:16:28 -0500

    fix spinner issue by replace window ready with on load

--- js/RootController.js
+++ js/RootController.js
@@ -1,6 +1,6 @@
 app.controller('RootController',['$scope','$window','$rootScope',function($scope,$window,$rootScope){
 	$scope.spinner = function() {
-		$(window).ready(function() {
+		$(window).on('load', function(){
 			$('.spinner').fadeOut(1000);
 		})
 	}
